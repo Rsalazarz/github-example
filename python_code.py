2c8c45e9bc048a6638dc3e97aa17497ec7410ca2
Rsalazarz <66881709+Rsalazarz@users.noreply.github.com>
2020-07-16 15:38:04 -0700

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,6 +1,5 @@
-vim python_code.py
 #this project will be used mainly for the capstone project
 import pandas as pd
 import Numpy as np
-print("Hello Github!")
+print("Hello Capstone Project Course!!")
 
